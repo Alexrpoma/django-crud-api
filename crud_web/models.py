4d6121167dd47bdacfaab741f7c18ad52b7dec5e
Alex Poma <alex.rpoma@outlook.com>
2023-08-26 19:27:49 -0400

    address model migrated.

--- crud_web/models.py
+++ crud_web/models.py
@@ -5,5 +5,14 @@ from django.db import models
 class Person(models.Model):
     username = models.CharField(max_length=10)
     nickname = models.CharField(max_length=10)
-    email = models.CharField(max_length=20)
+    email = models.CharField(max_length=20,  unique=True)
     password = models.CharField(max_length=20)
+
+
+class Address(models.Model):
+    house_number = models.IntegerField(unique=True)
+    street = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
