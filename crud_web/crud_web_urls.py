e356e62965fcf2cc3828f1ceedc250f61f4bfe45
Alex Poma <alex.rpoma@outlook.com>
2023-08-27 13:10:26 -0400

    get person api

--- crud_web/crud_web_urls.py
+++ crud_web/crud_web_urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('welcome', views.welcome_django),
     path('home', views.home_page),
     path('about-us', views.about_us),
-    path('person', views.persons),
-    path('person/<str:uuid>', views.person),
+    path('person', views.all_persons),
+    path('person/<str:uuid>', views.get_person),
     path('address', views.address)
 ]
--- crud_web/views.py
+++ crud_web/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Address, Person
+from django.shortcuts import get_object_or_404
+from django.core import serializers
 
 
 # Create your views here.
@@ -16,15 +18,33 @@ def about_us(request):
     return HttpResponse("<h2>About Us</h2>")
 
 
-def persons(request):
+def all_persons(request):
     persons_list = list(Person.objects.values())
     return JsonResponse(persons_list, safe=False)
 
 
-def person(request, uuid):
-    return HttpResponse(f'<h3>person id = {uuid}</h3>')
+def get_person(request, uuid):
+    person = get_object_or_404(Person, id=uuid)
+    person_data = {
+        'id': person.id,
+        'username': person.username,
+        'nickname': person.nickname,
+        'email': person.email
+    }
+    # person_serializable = serializers.serialize('json', person) --> option without mapping
+    return JsonResponse(person_data, safe=False)
 
 
 def address(request):
     address_list = list(Address.objects.values())
     return JsonResponse(address_list, safe=False)
+
+
+# def create_persons(request):
+#     person0 = Person(username='Anna', nickname='anni', email='anna@gmail.com', password='12313')
+#     person1 = Person(username='Carlos', nickname='carl', email='carlos@outlook.com', password='12313')
+#     person2 = Person(username='Robert', nickname='robi', email='robert@yahoo.com', password='12313')
+#     Person.save(person0)
+#     Person.save(person1)
+#     Person.save(person2)
+#     return HttpResponse('persons created!')
