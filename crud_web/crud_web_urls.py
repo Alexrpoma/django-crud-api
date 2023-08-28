96efd3a0fcec50e3564e36b0e608695c8d437954
Alex Poma <alex.rpoma@outlook.com>
2023-08-28 12:00:12 -0400

    complete default data added.

--- crud_web/crud_web_urls.py
+++ crud_web/crud_web_urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from crud_web import views
 
 urlpatterns = [
-    path('welcome', views.welcome_django),
-    path('home', views.home_page),
-    path('about-us', views.about_us),
+    path('', views.home_page),
+    path('about', views.about_us),
     path('person', views.all_persons),
     path('person/<str:uuid>', views.get_person),
-    path('address', views.address)
+    path('address', views.address),
+    path('create', views.default_data)
 ]
--- crud_web/views.py
+++ crud_web/views.py
@@ -6,12 +6,8 @@ from django.core import serializers
 
 
 # Create your views here.
-def welcome_django(request):
-    return HttpResponse("<h2>Welcome to django app</h2>")
-
-
 def home_page(request):
-    return HttpResponse("<h2>Home Page</h2>")
+    return HttpResponse("<h1>Welcome to Django!</h1>")
 
 
 def about_us(request):
@@ -40,11 +36,37 @@ def address(request):
     return JsonResponse(address_list, safe=False)
 
 
-# def create_persons(request):
-#     person0 = Person(username='Anna', nickname='anni', email='anna@gmail.com', password='12313')
-#     person1 = Person(username='Carlos', nickname='carl', email='carlos@outlook.com', password='12313')
-#     person2 = Person(username='Robert', nickname='robi', email='robert@yahoo.com', password='12313')
-#     Person.save(person0)
-#     Person.save(person1)
-#     Person.save(person2)
-#     return HttpResponse('persons created!')
+def default_data(request):
+
+    person0 = Person(username='Anna', nickname='annie', email='anna@gmail.com', password='12313')
+    person1 = Person(username='Carlos', nickname='carl', email='carlos@outlook.com', password='12313')
+    person2 = Person(username='Robert', nickname='robin', email='robert@yahoo.com', password='12313')
+
+    address0 = Address(house_number=133, street='Alamos', city='La Paz', country='Bolivia', person=person0)
+    address1 = Address(house_number=562, street='Rios', city='Quito', country='Ecuador', person=person1)
+    address2 = Address(house_number=485, street='Mercedes', city='Bogota', country='Colombia', person=person2)
+
+    Person.objects.bulk_create([person0, person1, person2])
+    Address.objects.bulk_create([address0, address1, address2])
+
+    return HttpResponse('persons created!')
+
+
+
+
+
+
+
+
+
+
+
+
+# A way to create persons objects:
+""" person_data = [
+         {'username':'Anna', 'nickname':'annie', 'email':'anna@gmail.com', 'password':'12313'},
+         {'username':'Carlos', 'nickname':'carl', 'email':'carlos@outlook.com', 'password':'12313'},
+         {'username':'Robert', 'nickname':'robin', 'email':'robert@yahoo.com', 'password':'12313'}
+     ]
+     for list_person in person_data:
+         Person.objects.create(**list_person) """
