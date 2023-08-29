bb87537fba326c9d7afcdc17b51c3b9fcc4bc5f6
Alex Poma <alex.rpoma@outlook.com>
2023-08-28 20:39:44 -0400

    get person format changed.

--- crud_web/models.py
+++ crud_web/models.py
@@ -3,24 +3,12 @@ from uuid import uuid4
 
 
 # Create your models here.
-class Person(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    username = models.CharField(max_length=10)
-    nickname = models.CharField(max_length=10)
-    email = models.CharField(max_length=20,  unique=True)
-    password = models.CharField(max_length=20)
-
-    def __str__(self):
-        return f'(id={self.id} username={self.username} nickname={self.nickname} email={self.email})'
-
-
 class Address(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     house_number = models.IntegerField(unique=True)
     street = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
     country = models.CharField(max_length=30)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
 
     def __str__(self):
         return (f'(id={self.id} '
@@ -29,3 +17,17 @@ class Address(models.Model):
                 f'city={self.city} '
                 f'country={self.country} '
                 f'person_id={self.person})')
+    
+
+class Person(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    username = models.CharField(max_length=10)
+    nickname = models.CharField(max_length=10)
+    email = models.CharField(max_length=20,  unique=True)
+    password = models.CharField(max_length=20)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'(id={self.id} username={self.username} nickname={self.nickname} email={self.email})'
+
+
